da70352497fbaf476d16bb76cadc5770f5b81178
bhattom756 <omsharma12022002@gmail.com>
2024-10-24 23:44:33 +0530

    ui: profile page addons

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,18 @@
-'use client';
-
-import { auth } from '../../context/Firebase';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Button from '@/components/Button';
+"use client";
+import { auth } from "../../context/Firebase";
+import React, { useEffect, useState } from "react";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import Image from "next/image";
 
 const Profile = () => {
-  const router = useRouter();
   const [userDetails, setUserDetails] = useState(null);
 
   const fetchUserData = async () => {
     auth.onAuthStateChanged(async (user) => {
-      console.log(user);
       setUserDetails(user);
+      console.log(user);
+      
     });
   };
 
@@ -20,29 +20,98 @@ const Profile = () => {
     fetchUserData();
   }, []);
 
-  async function handleLogout() {
-    try {
-      await auth.signOut();
-      router.replace('/');
-      console.log('logout success');
-    } catch (error) {
-      console.log('error logout', error.message);
-    }
-  }
-
   return (
-    <div>
-      {userDetails ? (
-        <div>
-          <h3>{userDetails.displayName}</h3>
-          <h3>{userDetails.email}</h3>
-          <Button className="btn btn-primary" onClick={handleLogout} text='Logout'/>
+    <>
+      <Navbar />
+      <div className="container p-5 mid-section">
+        <div className="d-flex align-items-start">
+          <div
+            className="nav flex-column nav-pills me-5 gap-5  rounded-4 p-3 shadow p-3 mb-5 bg-body-tertiary"
+            id="v-pills-tab"
+            role="tablist"
+            aria-orientation="vertical"
+          >
+            <button
+              className="nav-link active"
+              id="v-pills-home-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#v-pills-home"
+              type="button"
+              role="tab"
+              aria-controls="v-pills-home"
+              aria-selected="true"
+            >
+              Profile
+            </button>
+            <button
+              className="nav-link"
+              id="v-pills-profile-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#v-pills-profile"
+              type="button"
+              role="tab"
+              aria-controls="v-pills-profile"
+              aria-selected="false"
+            >
+              Coins
+            </button>
+            <button
+              className="nav-link"
+              id="v-pills-settings-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#v-pills-settings"
+              type="button"
+              role="tab"
+              aria-controls="v-pills-settings"
+              aria-selected="false"
+            >
+              Settings
+            </button>
+          </div>
+
+          <div className="tab-content shadow p-5 rounded-2 w-100" id="v-pills-tabContent">
+            {userDetails ? (
+              <>
+                <div
+                  className="tab-pane fade show active"
+                  id="v-pills-home"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-home-tab"
+                  tabIndex={0}
+                >
+                  <h3>{userDetails.displayName}</h3>
+                  <h3>{userDetails.email}</h3>
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="v-pills-profile"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-profile-tab"
+                  tabIndex={0}
+                >
+                  coins content
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="v-pills-settings"
+                  role="tabpanel"
+                  aria-labelledby="v-pills-settings-tab"
+                  tabIndex={0}
+                >
+                  <p>Settings Content...</p>
+                </div>
+              </>
+            ) : (
+              <div className="tab-pane fade show active" id="v-pills-home">
+                <p>Loading user data...</p>
+              </div>
+            )}
+          </div>
         </div>
-      ) : (
-        <p>Loading....</p>
-      )}
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
-export default Profile; 
+export default Profile;
